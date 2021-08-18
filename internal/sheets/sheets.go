cdbc409b07be82dbfd8210104e18a42e9602fad3
Brijesh Shetty <brij.jhu@gmail.com>
2021-08-18 00:05:47 -0700

    Add formatting logic for spreadsheet

--- internal/sheets/sheets.go
+++ internal/sheets/sheets.go
@@ -1,6 +1,8 @@
 package sheets
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
 
 	ts "github.com/brijs/teamsnap-team-gen/internal/teamsnap"
@@ -55,7 +57,9 @@ func (s *Service) PublishMatch(nextMatch ts.Event, teamA []*ts.Player, teamB []*
 	values = append(values, []interface{}{"Location", nextMatch.Location})
 	// hardcode 15 min earlier than startDate
 	loc, _ := time.LoadLocation("America/Los_Angeles")
-	values = append(values, []interface{}{"Date", nextMatch.StartDate.Add(time.Minute * -15).In(loc).String()})
+	sd := nextMatch.StartDate.Add(time.Minute * -15).In(loc)
+	values = append(values, []interface{}{"Date", sd.Format("Jan 2, 2006")})
+	values = append(values, []interface{}{"Reporting Time", sd.Format(time.Kitchen)})
 	values = append(values, []interface{}{"Uniform", nextMatch.Uniform})
 	values = append(values, []interface{}{"Note", nextMatch.Notes})
 
@@ -79,7 +83,7 @@ func (s *Service) PublishMatch(nextMatch ts.Event, teamA []*ts.Player, teamB []*
 	// team B
 	values = append(values, []interface{}{"Team " + teamBName, "Batting Order", "Bowling Order"})
 	for i, p := range teamB {
-		values = append(values, []interface{}{p.FirstName + " " + p.LastName, i + 1, len(teamA) - i})
+		values = append(values, []interface{}{p.FirstName + " " + p.LastName, i + 1, len(teamB) - i})
 	}
 	values = append(values, []interface{}{""})
 
@@ -91,6 +95,151 @@ func (s *Service) PublishMatch(nextMatch ts.Event, teamA []*ts.Player, teamB []*
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Format the sheets
+	var sheetId int // TODO: dynamically resolve these
+	switch groupName {
+	case "IntA":
+		sheetId = 660501010
+	case "IntB":
+		sheetId = 1959274600
+	case "IntC":
+		sheetId = 257356858
+	case "IntD":
+		sheetId = 1437245619
+	}
+	teamsRowStart := 5 + 2 + 2 + 1 + len(volunteers)
+	teamsRowEnd := teamsRowStart + len(teamA) + len(teamB) + 3
+	// clear all formatting
+	// background for top row
+	// Bold Location/Date/Time
+	// Borders around teams
+	r1 := []byte(fmt.Sprintf(`
+	{
+		"requests": [
+			{
+				"updateCells": {
+					"range": {
+						"sheetId": %d,
+						"startRowIndex": 5
+					},
+					"fields": "userEnteredFormat"
+				}
+			},
+			{
+				"repeatCell": {
+					"cell": {
+						"userEnteredFormat": {
+							"backgroundColor": {
+								"blue": 0.2,
+								"green": 0.2,
+								"red": 0.2
+							},
+							"horizontalAlignment": "CENTER",
+							"textFormat": {
+								"foregroundColor": {
+									"red": 1.0,
+									"green": 1.0,
+									"blue": 1.0
+								},
+								"bold": true
+							}
+						}
+					},
+					"fields": "userEnteredFormat(backgroundColor,horizontalAlignment,textFormat)",
+					"range": {
+						"sheetId": %d,
+						"startRowIndex": 0,
+						"endRowIndex": 1,
+						"startColumnIndex": 0,
+						"endColumnIndex": 1
+					}
+				}
+			},
+			{
+				"repeatCell": {
+					"cell": {
+						"userEnteredFormat": {
+							"textFormat": {
+								"bold": true
+							}
+						}
+					},
+					"fields": "userEnteredFormat(textFormat)",
+					"range": {
+						"sheetId": %d,
+						"startRowIndex": 1,
+						"endRowIndex": 4,
+						"startColumnIndex": 1,
+						"endColumnIndex": 2
+					}
+				}
+			},
+			{
+				"updateBorders": {
+					"range": {
+						"sheetId": %d,
+						"startRowIndex": %d,
+						"endRowIndex": %d,
+						"startColumnIndex": 0,
+						"endColumnIndex": 3
+					},
+					"top": {
+						"style": "SOLID",
+						"width": 1,
+						"color": {
+							"black": 1.0
+						}
+					},
+					"bottom": {
+						"style": "SOLID",
+						"width": 1,
+						"color": {
+							"black": 1.0
+						}
+					},
+					"left": {
+						"style": "SOLID",
+						"width": 1,
+						"color": {
+							"black": 1.0
+						}
+					},
+					"right": {
+						"style": "SOLID",
+						"width": 1,
+						"color": {
+							"black": 1.0
+						}
+					},
+					"innerHorizontal": {
+						"style": "SOLID",
+						"width": 1,
+						"color": {
+							"black": 1.0
+						}
+					},
+					"innerVertical": {
+						"style": "SOLID",
+						"width": 1,
+						"color": {
+							"black": 1.0
+						}
+					},
+					"fields": "*"
+				}
+			}
+		]
+	}	`, sheetId, sheetId, sheetId, sheetId, teamsRowStart, teamsRowEnd))
+	rr1 := &sheets.BatchUpdateSpreadsheetRequest{}
+	if err := json.Unmarshal(r1, rr1); err != nil {
+		log.Error(err)
+	}
+	_, err = s.srv.Spreadsheets.BatchUpdate(s.SpreadSheetID, rr1).Context(ctx).Do()
+	if err != nil {
+		log.Error(err)
+	}
+
 }
 
 func (s *Service) GetPreferredTeam(teamName string, players []*ts.Player) {
